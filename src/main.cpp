c592a95cf128ad138a63fc6022dd2b18bce03dad
M1l01 <emiliolemos2001@hotmail.com>
2025-06-25 19:23:42 -0500

    changing main to cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <driver/ledc.h>
+#include <esp_err.h>
+
+
 
 extern "C" void app_main(){
 
