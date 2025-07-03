0055147c12444cf6bfa41edfc471f0e3c391001e
M1l01 <emiliolemos2001@hotmail.com>
2025-07-02 22:42:23 -0500

    Testing rele circuit to control motor

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ extern "C" void app_main(){
     motor_in4_config.pin_bit_mask = (1ULL << PIN_IN4);
     motor_in4_config.mode = GPIO_MODE_OUTPUT;
     motor_in4_config.pull_up_en = GPIO_PULLUP_DISABLE;
-    motor_in4_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    motor_in4_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
     motor_in4_config.intr_type = GPIO_INTR_DISABLE;
     esp_err_t ret = gpio_config(&motor_in4_config);
     if (ret != ESP_OK) {
@@ -31,11 +31,23 @@ extern "C" void app_main(){
     motor_in3_config.pin_bit_mask = (1ULL << PIN_IN3);
     motor_in3_config.mode = GPIO_MODE_OUTPUT;
     motor_in3_config.pull_up_en = GPIO_PULLUP_DISABLE;
-    motor_in3_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    motor_in3_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
     motor_in3_config.intr_type = GPIO_INTR_DISABLE;
     ret = gpio_config(&motor_in3_config);
     if (ret != ESP_OK) {
         printf("Error configuring GPIO %d: %s\n", PIN_IN3, esp_err_to_name(ret));
+        return;
+    }
+
+    gpio_config_t motor_enb_config = {};
+    motor_enb_config.pin_bit_mask = (1ULL << PIN_ENB);
+    motor_enb_config.mode = GPIO_MODE_OUTPUT;
+    motor_enb_config.pull_up_en = GPIO_PULLUP_DISABLE;
+    motor_enb_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    motor_enb_config.intr_type = GPIO_INTR_DISABLE;
+    ret = gpio_config(&motor_enb_config);
+    if (ret != ESP_OK) {
+        printf("Error configuring GPIO %d: %s\n", PIN_ENB, esp_err_to_name(ret));
         return;
     }
 
@@ -44,7 +56,7 @@ extern "C" void app_main(){
         // Set motor IN4 high and IN3 low to move forward
         gpio_set_level(PIN_IN4, 1);
         gpio_set_level(PIN_IN3, 0);
-
+        gpio_set_level(PIN_ENB, 1);
     }
 
 }
