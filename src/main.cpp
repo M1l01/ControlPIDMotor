b0672e963a7b46b4a96e52d41485735120b2e6a1
M1l01 <emiliolemos2001@hotmail.com>
2025-07-04 21:32:42 -0500

    Implementing the PWM control with 12 BITS Timer resolution

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,25 @@
 #define PIN_ENC_A GPIO_NUM_36
 #define PIN_ENC_B GPIO_NUM_39
 
+#define TIMER LEDC_TIMER_0 // Timer que usara el PWM
+#define TIMER_BIT LEDC_TIMER_12_BIT
+#define FREQ_TIMER 10000
+#define TIMER_SPEED_MODE LEDC_HIGH_SPEED_MODE
+
+#define CHANNEL_EN LEDC_CHANNEL_0 // Channel for the PWM signal
+
+//Configurar el timer para PWM
+void config_timer(){
+    //Configuracion del Timer
+    ledc_timer_config_t ledcTimer = {};
+    ledcTimer.speed_mode = TIMER_SPEED_MODE;
+    ledcTimer.timer_num = TIMER;
+    ledcTimer.duty_resolution = TIMER_BIT;
+    ledcTimer.freq_hz = FREQ_TIMER;
+    ledcTimer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&ledcTimer);
+}
+
 extern "C" void app_main(){
     //Configuración de Perifericos
     gpio_config_t motor_turn_config = {};
@@ -32,16 +51,32 @@ extern "C" void app_main(){
     motor_enb_config.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&motor_enb_config);
 
+    // Configure the LEDC timer for PWM
+    config_timer();
+    // Configure the LEDC channel for the motor ENB
+    ledc_channel_config_t channel_config = {};
+    channel_config.gpio_num = PIN_ENB; // GPIO for PWM output
+    channel_config.speed_mode = TIMER_SPEED_MODE; // Speed mode for the channel
+    channel_config.channel = CHANNEL_EN; // Channel number
+    channel_config.timer_sel = TIMER; // Timer to use
+    channel_config.duty = 0; // Initial duty cycle (0 means off)
+    channel_config.hpoint = 0; // Hpoint value
+
+    ledc_channel_config(&channel_config); // Configure the channel
+
     //loop
     while(1){
         // Set motor ENB high
-        gpio_set_level(PIN_ENB, 1);
+        ledc_set_duty(TIMER_SPEED_MODE, CHANNEL_EN, 4095); // Set duty cycle to maximum (100%)
+        ledc_update_duty(TIMER_SPEED_MODE, CHANNEL_EN); // Update the duty cycle
+        
         // Set motor TURN high (CCW)
         gpio_set_level(PIN_TURN, 1);
+        /*
         vTaskDelay(pdMS_TO_TICKS(1000)); // Wait for 1 second
         // Set motor TURN low (CW)
         gpio_set_level(PIN_TURN, 0);
         vTaskDelay(pdMS_TO_TICKS(1000)); // Wait for 1 second
+        */
     }
-
 }
