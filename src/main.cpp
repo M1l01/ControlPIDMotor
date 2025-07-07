1a194359d72ce8560dfb5a3f615fc3598be26f7e
M1l01 <emiliolemos2001@hotmail.com>
2025-07-07 15:32:05 -0500

    Measuring encoder data to identify direction and revolutions of motor

--- src/main.cpp
+++ src/main.cpp
@@ -21,40 +21,39 @@
 
 #define CHANNEL_EN LEDC_CHANNEL_0 // Channel for the PWM signal
 
-//Configurar el timer para PWM
-void config_timer(){
-    //Configuracion del Timer
-    ledc_timer_config_t ledcTimer = {};
-    ledcTimer.speed_mode = TIMER_SPEED_MODE;
-    ledcTimer.timer_num = TIMER;
-    ledcTimer.duty_resolution = TIMER_BIT;
-    ledcTimer.freq_hz = FREQ_TIMER;
-    ledcTimer.clk_cfg = LEDC_AUTO_CLK;
-    ledc_timer_config(&ledcTimer);
-}
+// Global Variables
 
+//Variables to handle the encoder detection
+int16_t encoder_count = 0; // Variable to count encoder ticks (11 ticks per revolution)
+bool encoder_direction = true; // Variable to identify the direction of the motor (true for CCW, false for CW)
+
+//-------- Function Prototypes --------
+esp_err_t init_motor_gpio(gpio_num_t control_pin);
+void init_timer();
+esp_err_t init_irs(void);
+void isr_handler(void *arg);
+
+//-------- Main Function --------
 extern "C" void app_main(){
-    //Configuración de Perifericos
-    // Configure the GPIO for motor TURN
-    gpio_config_t motor_turn_config = {};
-    motor_turn_config.pin_bit_mask = (1ULL << PIN_TURN);
-    motor_turn_config.mode = GPIO_MODE_OUTPUT;
-    motor_turn_config.pull_up_en = GPIO_PULLUP_DISABLE;
-    motor_turn_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    motor_turn_config.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&motor_turn_config);
+    // Configuración de Perifericos
 
-    //Configure the GPIO for motor PWM control (ENB)
-    gpio_config_t motor_enb_config = {};
-    motor_enb_config.pin_bit_mask = (1ULL << PIN_ENB);
-    motor_enb_config.mode = GPIO_MODE_OUTPUT;
-    motor_enb_config.pull_up_en = GPIO_PULLUP_DISABLE;
-    motor_enb_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    motor_enb_config.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&motor_enb_config);
+    // Configure the GPIO for motor TURN
+    init_motor_gpio(PIN_TURN);
+
+    // Configure the GPIO for motor PWM control (ENB)
+    init_motor_gpio(PIN_ENB);
+
+    // Configure the GPIO for encoder channel B
+    gpio_config_t enc_b_config = {};
+    enc_b_config.pin_bit_mask = (1ULL << PIN_ENC_B);
+    enc_b_config.mode = GPIO_MODE_INPUT; // Set as input
+    enc_b_config.pull_up_en = GPIO_PULLUP_ENABLE; // Enable pull-up resistor
+    enc_b_config.pull_down_en = GPIO_PULLDOWN_DISABLE; // Disable pull-down resistor
+    gpio_config(&enc_b_config);
 
     // Configure the LEDC timer for PWM
-    config_timer();
+    init_timer();
+
     // Configure the LEDC channel for the motor ENB
     ledc_channel_config_t channel_config = {};
     channel_config.gpio_num = PIN_ENB; // GPIO for PWM output
@@ -63,28 +62,85 @@ extern "C" void app_main(){
     channel_config.timer_sel = TIMER; // Timer to use
     channel_config.duty = 0; // Initial duty cycle (0 means off)
     channel_config.hpoint = 0; // Hpoint value
-
     ledc_channel_config(&channel_config); // Configure the channel
+
+    // Initialize the encoder interrupt of channel A
+    init_irs();
 
     //loop
     while(1){
         // Set motor TURN high (CCW)
         gpio_set_level(PIN_TURN, 1);
         
-        // Set motor ENB high
-        ledc_set_duty(TIMER_SPEED_MODE, CHANNEL_EN, 4095); // Set duty cycle to maximum (100%)
-        ledc_update_duty(TIMER_SPEED_MODE, CHANNEL_EN); // Update the duty cycle
-        
-        vTaskDelay(pdMS_TO_TICKS(2000)); // Wait for 2 seconds
-
-        ledc_set_duty(TIMER_SPEED_MODE, CHANNEL_EN, 3276); // Set duty cycle to maximum (80%)
-        ledc_update_duty(TIMER_SPEED_MODE, CHANNEL_EN); // Update the duty cycle
-
-        vTaskDelay(pdMS_TO_TICKS(2000)); // Wait for 2 seconds
-
+        // Set motor ENB high in 60% duty cycle
         ledc_set_duty(TIMER_SPEED_MODE, CHANNEL_EN, 2457); // Set duty cycle to maximum (60%)
         ledc_update_duty(TIMER_SPEED_MODE, CHANNEL_EN); // Update the duty cycle
 
-        vTaskDelay(pdMS_TO_TICKS(2000)); // Wait for 2 seconds
+        //Print the encoder count and direction
+        printf("Encoder Count: %d\n", encoder_count);
+        printf("Revoluciones: %d\n", encoder_count / 11);
+        printf("Direction: %s\n", encoder_direction ? "CCW" : "CW");
+
+        vTaskDelay(pdMS_TO_TICKS(10)); // Whatchdog delay to prevent task starvation
+    }
+}
+
+//-------- Function Definitions --------
+
+// Initilize the GPIOs for the control of the motor
+esp_err_t init_motor_gpio(gpio_num_t control_pin) {
+    gpio_config_t motor_turn_config = {};
+    motor_turn_config.pin_bit_mask = (1ULL << control_pin);
+    motor_turn_config.mode = GPIO_MODE_OUTPUT;
+    motor_turn_config.pull_up_en = GPIO_PULLUP_DISABLE;
+    motor_turn_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    motor_turn_config.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&motor_turn_config);
+    return ESP_OK;
+}
+
+//Configurar el timer para PWM
+void init_timer(){
+    //Configuracion del Timer
+    ledc_timer_config_t ledcTimer = {};
+    ledcTimer.speed_mode = TIMER_SPEED_MODE; 
+    ledcTimer.timer_num = TIMER;
+    ledcTimer.duty_resolution = TIMER_BIT;
+    ledcTimer.freq_hz = FREQ_TIMER;
+    ledcTimer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&ledcTimer);
+}
+
+// Initialize the interrput to lecture of encoders
+esp_err_t init_irs(void) {
+    gpio_config_t enc_a_config = {};
+    enc_a_config.pin_bit_mask = (1ULL << PIN_ENC_A);
+    enc_a_config.mode = GPIO_MODE_INPUT;
+    enc_a_config.pull_up_en = GPIO_PULLUP_ENABLE;
+    enc_a_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    enc_a_config.intr_type = GPIO_INTR_POSEDGE; // Interrupt on rising edge
+    gpio_config(&enc_a_config);
+
+    // Installing the interrupts service
+    gpio_install_isr_service(0);
+
+    gpio_isr_handler_add(PIN_ENC_A, isr_handler, NULL);
+
+    return ESP_OK;
+}
+
+// Function to count encoder tiks and identify direction
+// This function will be called in the ISR
+void isr_handler(void *arg) {
+    //Increment the encoder count
+    encoder_count++;
+    // Check the state of channel B to determine direction
+    int level_b = gpio_get_level(PIN_ENC_B);
+    if (level_b == 1) {
+        // If channel B is high, the direction is CCW
+        encoder_direction = true;
+    } else {
+        // If channel B is low, the direction is CW
+        encoder_direction = false;
     }
 }
